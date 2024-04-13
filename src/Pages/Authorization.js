a385cc152b47d5134e15d6bce185aec6345c6252
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2024-04-14 04:35:23 +0800

    Style the Authorization Page

--- src/Pages/Authorization.js
+++ src/Pages/Authorization.js
@@ -8,26 +8,32 @@ const Authorization = () => {
     const {projectID, redirectURL, scope, authToken} = location.state
     return (
         <div className='flex flex-col items-center'>
-            <div className='flex flex-col items-center justify-center bg-gray-100 rounded-lg shadow-md p-10 transition-transform w-96 text-center mt-20'>
-                <div>
-                    <h1>An external application</h1>
-                    <h1>{projectID}</h1>
-                    <h1>wants to access your HKID account</h1>
+            <div className='flex flex-col  bg-gray-100 rounded-lg shadow-md p-10 transition-transform w-1/3 mt-20 '>
+                <div className='flex flex-col items-center justify-center text-base' >
+                    <h1 className='text-gray-500'>An external application</h1>
+                    <h1 className='font-bold text-xl'>{projectID.split(".")[0].toUpperCase()}</h1>
+                    <h1 className='text-gray-500'>wants to access your HKID account</h1>
                 </div>
+                <br/>
                 <hr/>
-                <div>
-                    <h3>THIS WILL ALLOW THE DEVELOPERS OF {projectID} TO:</h3>
-                    <h3>Access your name, hkid</h3>
-                    <h3>Access your email address</h3>
-                    <h3>Access your password or record a new mixtape</h3>
+                <br/>
+                <div className='text-sm items-start space-y-2'>
+                    <h1 className=''>THIS WILL ALLOW THE DEVELOPERS OF {projectID.split(".")[0].toUpperCase()} TO:</h1>
+                    <h3>- Access your name, hkid</h3>
+                    <h3>- Access your email address</h3>
+                    <h3>- Access your password or record a new mixtape</h3>
                 </div>
-                <hr />
-                <div>
+                <br/>
+                <hr/>
+                <br/>
+                <div className='text-xs items-start space-y-2'>
                     <h5>Once you authorize, you will be redirected outside of HKID to: {redirectURL}</h5>
                     <h5>The developer of {projectID}'s privacy policy and terms of service apply to this application.</h5>
                 </div>
-                
-                <div className='flex space-x-14'>
+                <br/>
+        
+                <br/>
+                <div className='flex space-x-16 justify-between'>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         Cancel
                     </button>
