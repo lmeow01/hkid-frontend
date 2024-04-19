4dab062412683c93e32bce9a7a5e3aeb14b76f74
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2024-04-20 06:21:12 +0800

    refill project details after login

--- src/Pages/Authorization.js
+++ src/Pages/Authorization.js
@@ -73,7 +73,7 @@ const Authorization = () => {
                 <div className='flex space-x-16 justify-between'>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => {
                       removeCookie("authToken", { path: "/" });
-                      window.location.href = "/";
+                      window.location.href = "/?projectID=${projectID}&redirectURL=${redirectURL}&scope=${scope}";
                     }}>
                         Cancel
                     </button>
