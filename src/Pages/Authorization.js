0606eb689acc672613dc8f6c6be86bbcef402feb
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2024-04-20 06:07:11 +0800

    added remove cookie on cancel button and remove adding cookie bug

--- src/Pages/Authorization.js
+++ src/Pages/Authorization.js
@@ -73,7 +73,9 @@ const Authorization = () => {
         
                 <br/>
                 <div className='flex space-x-16 justify-between'>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => {
+                      removeCookie("authToken", { path: "/" });
+                    }}>
                         Cancel
                     </button>
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={async () => {
