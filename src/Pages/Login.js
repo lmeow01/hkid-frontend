d3aeeef195c319de2708eebb024751ab0f0b0d58
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2024-04-20 01:34:30 +0800

    Commenting out password section

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -82,7 +82,7 @@ const Login = () => {
                         required
                     />
 
-                    <label
+                    {/* <label
                         for="password"
                         class="block mb-2 text-left text-gray-700 font-bold"
                     >Password:</label>
@@ -97,7 +97,7 @@ const Login = () => {
                         placeholder="Enter your Password"
                         class="block w-full mb-6 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-green-400"
                         required
-                    />
+                    /> */}
 
                     <div class="flex justify-center items-center">
                         <button
